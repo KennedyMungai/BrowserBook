253e1a8e11865bb88dce4dacff910a57fd80315d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-23 15:51:22 +0300

    Removed the else block for the build.onload function

--- browser-transpiler/src/plugins/unpkg-path-plugin.ts
+++ browser-transpiler/src/plugins/unpkg-path-plugin.ts
@@ -22,12 +22,6 @@ export const unpkgPathPlugin = () => {
             `,
           };
         }
-        else {
-            return {
-                loader: 'jsx',
-                contents: 'export default "Hi there!"',
-            }
-        }
       });
     },
   };
