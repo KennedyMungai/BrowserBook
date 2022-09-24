7528f1a10c828b014babc75115c1722fafe79e9c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-24 12:00:44 +0300

    Added an import for react and console logged it

--- browser-transpiler/src/plugins/unpkg-path-plugin.ts
+++ browser-transpiler/src/plugins/unpkg-path-plugin.ts
@@ -36,9 +36,8 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const react = require('react');
-              const reactDOM = require('react-dom');
-              console.log(react, reactDOM);
+              import React from 'react';
+              console.log(React);
             `,
           };
         }
