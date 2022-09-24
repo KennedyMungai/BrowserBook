b3aede11d5f93c58d8aa9fb97cf46ea2ab782dc2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-24 11:59:28 +0300

    Updated the contents returned by the build.onLoad function

--- browser-transpiler/src/plugins/unpkg-path-plugin.ts
+++ browser-transpiler/src/plugins/unpkg-path-plugin.ts
@@ -36,8 +36,9 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('nested-test-pkg');
-              console.log(message);
+              const react = require('react');
+              const reactDOM = require('react-dom');
+              console.log(react, reactDOM);
             `,
           };
         }
