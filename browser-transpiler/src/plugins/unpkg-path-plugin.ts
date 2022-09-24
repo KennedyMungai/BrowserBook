546efda5bf338310ada813d7af7a6a6abbc39253
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-24 09:59:30 +0300

    Only retirn the path object of the args.path is index.js

--- browser-transpiler/src/plugins/unpkg-path-plugin.ts
+++ browser-transpiler/src/plugins/unpkg-path-plugin.ts
@@ -7,7 +7,10 @@ export const unpkgPathPlugin = () => {
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         console.log('onResolve', args);
-        return { path: args.path, namespace: 'a' };
+
+        if (args.path === 'index.js') {
+          return { path: args.path, namespace: 'a' };
+        }
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
