ce3645ad5164d71b57f4d36212a812d204eb2ac1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-28 15:49:35 +0300

    Hardcoded the loader to jsx

--- browser-transpiler/src/plugins/fetch-plugin.ts
+++ browser-transpiler/src/plugins/fetch-plugin.ts
@@ -28,10 +28,8 @@ export const fetchPlugin = (inputCode: string) => {
 
                 const { data, request } = await axios.get(args.path);
 
-                const loader = args.path.match(/.css$/) ? 'css' : 'jsx' ;
-
                 const result: esbuild.OnLoadResult = {
-                    loader,
+                    loader: 'jsx',
                     contents: data,
                     resolveDir: new URL('./', request.responseURL).pathname
                 };
