64fce0533e16f16c061ef6a1516e048d6ae4b484
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-28 15:19:06 +0300

    Added some new code to the fetch-plugin ts file

--- browser-transpiler/src/plugins/fetch-plugin.ts
+++ browser-transpiler/src/plugins/fetch-plugin.ts
@@ -1,27 +1,37 @@
-build.onLoad({ filter: /.*/ }, async (args: any) => {
-    if (args.path === 'index.js') {
-      return {
-        loader: 'jsx',
-        contents: inputCode,
-      };
+
+
+
+export const fetchPlugin = () => {
+    return {
+        name: 'fetch-plugin',
+        setup(build: esbuild.PluginBuild) {
+            build.onLoad({ filter: /.*/ }, async (args: any) => {
+                if (args.path === 'index.js') {
+                    return {
+                        loader: 'jsx',
+                        contents: inputCode,
+                    };
+                }
+
+                const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+
+                if (cachedResult) {
+                    return cachedResult;
+                }
+
+                const { data, request } = await axios.get(args.path);
+
+                const result: esbuild.OnLoadResult = {
+                    loader: 'jsx',
+                    contents: data,
+                    resolveDir: new URL('./', request.responseURL).pathname
+                };
+
+                // Store the response in cache
+                await fileCache.setItem(args.path, result);
+
+                return result;
+            });
+        }
     }
-
-    const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
-
-    if (cachedResult) {
-      return cachedResult;
-    }
-
-    const { data, request } = await axios.get(args.path);
-
-    const result: esbuild.OnLoadResult = {
-      loader: 'jsx',
-      contents: data,
-      resolveDir: new URL('./', request.responseURL).pathname
-    };
-
-    // Store the response in cache
-    await fileCache.setItem(args.path, result);
-
-    return result;
-  });
+};
