349626ed4573b840c78dd98369437063320f7635
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-28 15:16:33 +0300

    Moved the build.onLoad function

--- browser-transpiler/src/plugins/unpkg-path-plugin.ts
+++ browser-transpiler/src/plugins/unpkg-path-plugin.ts
@@ -32,33 +32,7 @@ export const unpkgPathPlugin = (inputCode: string) => {
         }
       });
 
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: inputCode,
-          };
-        }
-
-        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
-
-        if (cachedResult) {
-          return cachedResult;
-        }
-
-        const { data, request } = await axios.get(args.path);
-
-        const result: esbuild.OnLoadResult = {
-          loader: 'jsx',
-          contents: data,
-          resolveDir: new URL('./', request.responseURL).pathname
-        };
-
-        // Store the response in cache
-        await fileCache.setItem(args.path, result);
-
-        return result;
-      });
+      
     },
   };
 };
