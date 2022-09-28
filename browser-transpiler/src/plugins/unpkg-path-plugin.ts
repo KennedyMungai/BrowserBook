d41ff04b295b3e6c1888486b731e136043d07590
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-28 13:19:05 +0300

    Created an instance of local forage called fileCache

--- browser-transpiler/src/plugins/unpkg-path-plugin.ts
+++ browser-transpiler/src/plugins/unpkg-path-plugin.ts
@@ -3,6 +3,10 @@ import * as esbuild from 'esbuild-wasm';
 import localforage from 'localforage';
 
 
+const fileCache = localforage.createInstance({
+  name: 'filecache'
+});
+
 export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
