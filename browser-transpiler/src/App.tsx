9702c60bb975f3bc6dcfec82a6470699b64c35fc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-04 12:20:58 +0300

    Added the title element to the iframe tag

--- browser-transpiler/src/App.tsx
+++ browser-transpiler/src/App.tsx
@@ -56,7 +56,7 @@ function App() {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
-      <iframe sandbox='' srcDoc={html} />
+      <iframe sandbox='' srcDoc={html} title="Script window" />
     </div>
   );
 }
