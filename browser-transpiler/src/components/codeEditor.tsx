913abd50a0b3c8a7133f5267875e59fe5056ca8f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-05 14:11:42 +0300

    Gave the MonacoEditor a height of 500px

--- browser-transpiler/src/components/codeEditor.tsx
+++ browser-transpiler/src/components/codeEditor.tsx
@@ -3,7 +3,7 @@ import MonacoEditor from '@monaco-editor/react';
 
 
 const CodeEditor = () => {
-    return <MonacoEditor />
+    return <MonacoEditor height='500px' />
 }
 
 export default CodeEditor;
