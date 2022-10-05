85a09c7d626f11ca720849dd39cefdad3e76e724
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-05 15:26:27 +0300

    Performed some code refactoring and added a formatting button for the code

--- browser-transpiler/src/components/codeEditor.tsx
+++ browser-transpiler/src/components/codeEditor.tsx
@@ -17,23 +17,31 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
         });
     };
 
-    return <MonacoEditor
-        editorDidMount={onEditorDidMount}
-        height='500px'
-        theme='vs-dark'
-        language='javascript'
-        options={{
-            wordWrap: 'on',
-            minimap: { enabled: false },
-            showUnused: false,
-            folding: false,
-            lineNumbersMinChars: 3,
-            fontSize: 16,
-            scrollBeyondLastLine: false,
-            automaticLayout: true
-        }}
-        value={initialValue}
-    />
+    return (
+        <>
+            <div className="">
+                <button>Format</button>
+            </div>
+
+            <MonacoEditor
+                editorDidMount={onEditorDidMount}
+                height='500px'
+                theme='vs-dark'
+                language='javascript'
+                options={{
+                    wordWrap: 'on',
+                    minimap: { enabled: false },
+                    showUnused: false,
+                    folding: false,
+                    lineNumbersMinChars: 3,
+                    fontSize: 16,
+                    scrollBeyondLastLine: false,
+                    automaticLayout: true
+                }}
+                value={initialValue}
+            />
+        </>
+    );
 }
 
 export default CodeEditor;
