368b98c6bff59bd80a2455ddbcf5c4b3566e0a2b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-05 14:45:18 +0300

    Set the initial value for the code editor

--- browser-transpiler/src/App.tsx
+++ browser-transpiler/src/App.tsx
@@ -72,7 +72,7 @@ function App() {
 
   return (
     <div>
-      <CodeEditor />
+      <CodeEditor initialValue='const a = 1' />
       <textarea onChange={(e) => setInput(e.target.value)} value={input}></textarea>
       <div className="">
         <button onClick={onClick}>Submit</button>
