831e7ed6cbb3211ac069a5788a52763e3630379e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-06 14:54:27 +0300

    Made the anonymous arrow function asynchronous

--- browser-transpiler/src/bundler/index.ts
+++ browser-transpiler/src/bundler/index.ts
@@ -3,7 +3,7 @@ import { unpkgPathPlugin } from '../plugins/unpkg-path-plugin';
 import { fetchPlugin } from '../plugins/fetch-plugin';
 
 
-export default (rawCode: string) => {
+export default async (rawCode: string) => {
     await esbuild.startService({
         worker: true,
         wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
