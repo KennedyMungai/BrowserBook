9d6e08e88051686fe0c5536bf06a13cba9cf69dd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-06 15:29:30 +0300

    Replaced the code inside the code-cell component

--- browser-transpiler/src/components/code-cell.tsx
+++ browser-transpiler/src/components/code-cell.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CodeEditor from './components/codeEditor';
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import Preview from './components/Preview';
+import bundle from './bundler';
 
 
-const codeCell = () => {
+function codeCell() {
+    const [code, setCode] = useState<string>('');
+    const [input, setInput] = useState<string>('');
+
+    const onClick = async () => {
+        const output = await bundle(input);
+        setCode(output);
+    };
+
     return (
-        <div>code-cell</div>
-    )
+        <div>
+            <CodeEditor
+                initialValue='const a = 1'
+                onChange={(value) => setInput(value)}
+            />
+
+            <div className="">
+                <button onClick={onClick}>Submit</button>
+            </div>
+            <Preview code={code} />
+        </div>
+    );
 }
 
 export default codeCell;
