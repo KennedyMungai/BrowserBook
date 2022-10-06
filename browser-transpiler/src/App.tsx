403a7191776a1cbb88067575ebb9fb752fd0557d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-06 15:38:22 +0300

    Added the codeCell component

--- browser-transpiler/src/App.tsx
+++ browser-transpiler/src/App.tsx
@@ -4,25 +4,9 @@ import codeCell from './components/code-cell';
 
 
 function App() {
-  const [code, setCode] = useState<string>('');
-  const [input, setInput] = useState<string>('');
-
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
-
   return (
     <div>
-      <CodeEditor
-        initialValue='const a = 1'
-        onChange={(value) => setInput(value)}
-      />
-
-      <div className="">
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <Preview code={code} />
+      <codeCell />
     </div>
   );
 }
