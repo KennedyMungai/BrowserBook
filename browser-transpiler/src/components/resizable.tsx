e49a30480d9a60f7f865e7a6e85e20c72fafc394
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-11 13:58:47 +0300

    Added width as the dependency for the useEffect hook

--- browser-transpiler/src/components/resizable.tsx
+++ browser-transpiler/src/components/resizable.tsx
@@ -40,7 +40,7 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
             window.removeEventListener('resize', listener);
         };
 
-    }, []);
+    }, [width]);
 
 
     if (direction === 'horizontal') {
