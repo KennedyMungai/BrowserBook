de7098207d42660f0fd2729f14f8f94a33dcd98a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-11 14:53:52 +0300

    Gave the div a className of preview error

--- browser-transpiler/src/components/Preview.tsx
+++ browser-transpiler/src/components/Preview.tsx
@@ -58,7 +58,7 @@ const Preview: React.FC<PreviewProps> = ({ code, err }) => {
       title="Script window"
       ref={iframe}
     />
-    {err && <div>{err}</div>}
+    {err && <div className='preview-error'>{err}</div>}
   </div>;
 }
 
