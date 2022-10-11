38ae4adbe28e68514678c362c66032221a4d56d2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-11 12:25:41 +0300

    Set the state values to handle the constreaints instead of system variables

--- browser-transpiler/src/components/resizable.tsx
+++ browser-transpiler/src/components/resizable.tsx
@@ -33,18 +33,18 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
         resizableProps = {
             className: 'resize-horizontal',
             height: Infinity,
-            width: window.innerWidth * 0.75,
+            width: innerWidth * 0.75,
             resizeHandles: ['e'],
-            maxConstraints: [window.innerWidth * 0.75, Infinity],
-            minConstraints: [window.innerWidth * 0.2, Infinity]
+            maxConstraints: [innerWidth * 0.75, Infinity],
+            minConstraints: [innerWidth * 0.2, Infinity]
         }
     } else {
         resizableProps = {
             height: 300,
             width: Infinity,
             resizeHandles: ['s'],
-            maxConstraints: [Infinity, window.innerHeight * 0.9],
-            minConstraints: [Infinity, window.innerHeight * 0.15]
+            maxConstraints: [Infinity, innerHeight * 0.9],
+            minConstraints: [Infinity, innerHeight * 0.15]
         }
     }
 
