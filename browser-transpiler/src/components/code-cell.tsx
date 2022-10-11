548e072f5aa90488098acbd515b17d7351c1b06a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-11 14:01:50 +0300

    Added an import for the useEffect hook

--- browser-transpiler/src/components/code-cell.tsx
+++ browser-transpiler/src/components/code-cell.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CodeEditor from './codeEditor';
 import Preview from './Preview';
 import bundle from '../bundler';
