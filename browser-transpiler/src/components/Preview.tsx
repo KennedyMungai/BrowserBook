c2b279856c2f24b2f40867c0f63f82d69a826407
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-11 14:27:29 +0300

    Moved all the logic inside the catch statement to the arrow function

--- browser-transpiler/src/components/Preview.tsx
+++ browser-transpiler/src/components/Preview.tsx
@@ -17,16 +17,16 @@ const html = `
 
       <script>
         const handleError = (err) => {
-
+            const root = document.querySelector('#root');
+            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err +'</div>';
+            console.error(err);
         };
 
         window.addEventListener('message', (event) => {
           try {
             eval(event.data);
           } catch (err) {
-            const root = document.querySelector('#root');
-            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err +'</div>';
-            console.error(err);
+            
           }
         }, false);
       </script>
