56cd8c20aa6e35e7575a72d6b33ccf0d69a99d8b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-12 14:19:01 +0300

    Added an import for the store

--- browser-transpiler/src/App.tsx
+++ browser-transpiler/src/App.tsx
@@ -3,6 +3,7 @@ import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import CodeCell from './components/code-cell';
 import TextEditor from './components/text-editor';
 import { Provider } from 'react-redux';
+import { store } from './state';
 
 
 function App() {
