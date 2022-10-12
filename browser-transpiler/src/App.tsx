40353f517930b5af0d40e11350cba880d21fdf72
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-12 11:16:23 +0300

    Added an import for thr TextEditor react component

--- browser-transpiler/src/App.tsx
+++ browser-transpiler/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import CodeCell from './components/code-cell';
+import TextEditor from './components/text-editor';
 
 
 function App() {
