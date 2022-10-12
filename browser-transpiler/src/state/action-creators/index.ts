f0ba46fd45cda1828b77bcff0296569c22f98756
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-12 14:42:45 +0300

    Added the appropriate logic to the updateCell arrow function

--- browser-transpiler/src/state/action-creators/index.ts
+++ browser-transpiler/src/state/action-creators/index.ts
@@ -2,7 +2,15 @@ import { ActionType } from "../action-types";
 import { DeleteCellAction, InsertCellBeforeAction, MoveCellAction, UpdateCellAction } from '../actions';
 
 
-export const updateCell = (): UpdateCellAction => { };
+export const updateCell = (id: string, content: string): UpdateCellAction => {
+    return {
+        type: ActionType.UPDATE_CELL,
+        payload: {
+            id,
+            content
+        }
+    }
+};
 
 export const deleteCell = (): DeleteCellAction => { };
 
