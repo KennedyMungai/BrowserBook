121f1901b2c7bbe6f9a623a125a9f7dd868946c0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-12 14:09:33 +0300

    Created the reducers constant which holds the combineReducers function and exported it

--- browser-transpiler/src/state/reducers/index.ts
+++ browser-transpiler/src/state/reducers/index.ts
@@ -1,3 +1,9 @@
 import { combineReducers } from "redux";
-import reducer from "./cellsReducer";
+import cellsReducer from "./cellsReducer";
 
+
+const reducers = combineReducers({
+    cells: cellsReducer,
+});
+
+export default reducers;
