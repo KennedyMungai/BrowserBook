5018a568c3d3c22bcbaf8b0a3de52f8fe698c535
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-12 14:51:19 +0300

    Changed the code accordingly

--- browser-transpiler/src/state/actions/index.ts
+++ browser-transpiler/src/state/actions/index.ts
@@ -1,12 +1,12 @@
 import { ActionType } from "../action-types";
-import { CellTypes } from '../cell';
+import { CellTypes, Direction } from '../cell';
 
 
 export interface MoveCellAction {
     type: ActionType.MOVE_CELL;
     payload: {
         id: string,
-        direction: 'up' | 'down';
+        direction: Direction;
     }
 }
 
