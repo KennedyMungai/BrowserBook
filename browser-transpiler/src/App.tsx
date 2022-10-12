cf8f939958f398f3335aaeecf6c47e8e30d2ebb6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-12 14:57:54 +0300

    Properly removed the import statement

--- browser-transpiler/src/App.tsx
+++ browser-transpiler/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import CodeCell from './components/code-cell';
-import from './components/text-editor';
 import { Provider } from 'react-redux';
 import { store } from './state';
 
