a4fb546a698d233046cd6c55d23d0930afb7a951
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-12 14:18:36 +0300

    Added an import for Provider from react-redux

--- browser-transpiler/src/App.tsx
+++ browser-transpiler/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import CodeCell from './components/code-cell';
 import TextEditor from './components/text-editor';
+import { Provider } from 'react-redux';
 
 
 function App() {
