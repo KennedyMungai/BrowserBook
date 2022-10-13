7e8d8b742c9f3c0583689e0231e03b4a73ad654c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-13 14:57:03 +0300

    Imported the styling code

--- browser-transpiler/src/components/cell-list-item.tsx
+++ browser-transpiler/src/components/cell-list-item.tsx
@@ -3,6 +3,7 @@ import { Cell } from '../state';
 import ActionBar from './action-bar';
 import CodeCell from './code-cell';
 import TextEditor from './text-editor';
+import './cell-list-item.css';
 
 
 interface CellListItemProps {
