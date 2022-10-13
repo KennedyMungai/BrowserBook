aaa491031a5ed2de8d5e27b71504e8e96763ee48
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-13 08:29:28 +0300

    Rendered out the Cell List component

--- browser-transpiler/src/App.tsx
+++ browser-transpiler/src/App.tsx
@@ -3,13 +3,14 @@ import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import CodeCell from './components/code-cell';
 import { Provider } from 'react-redux';
 import { store } from './state';
+import CellList from './components/cell-list';
 
 
 function App() {
   return (
     <Provider store={store}>
       <div>
-        <CodeCell />
+        <CellList />
       </div>
     </Provider>
 
