705472a1df4719c4629c4b0f736f4536ecfae8da
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-13 08:16:02 +0300

    Dispatched the INSERT_CELL_BEFORE action

--- browser-transpiler/src/state/store.ts
+++ browser-transpiler/src/state/store.ts
@@ -5,3 +5,11 @@ import reducers from "./reducers";
 
 
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+store.dispatch({
+    type: ActionType.INSERT_CELL_BEFORE,
+    payload: {
+        id: null,
+        type: 'code',
+    }
+});
