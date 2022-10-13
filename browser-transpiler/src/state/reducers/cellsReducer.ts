3ece2573e896774eec89a56f6c3603be93fe869e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-13 07:40:51 +0300

    Added code to take care of indices outside the array bounds

--- browser-transpiler/src/state/reducers/cellsReducer.ts
+++ browser-transpiler/src/state/reducers/cellsReducer.ts
@@ -36,6 +36,9 @@ const cellsReducer = produce((state: CellsState = initialState, action: Action):
             const { direction } = action.payload;
             const index = state.order.findIndex((id) => id === action.payload.id);
             const targetIndex = direction === 'up' ? index - 1 : index + 1;
+            if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+                return;
+            }
             return state;
         case ActionType.INSERT_CELL_BEFORE:
             return state;
