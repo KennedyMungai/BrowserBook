3594bc23623c0956220e3ceea473b356525e2cd9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-13 08:41:21 +0300

    Destructured the code inside the useTypedSelector

--- browser-transpiler/src/components/cell-list.tsx
+++ browser-transpiler/src/components/cell-list.tsx
@@ -3,7 +3,9 @@ import { useTypedSelector } from '../hooks/use-typed-selector';
 
 
 const CellList: React.FC = () => {
-    useTypedSelector((state) => state);
+    useTypedSelector(({ cells: { order, data } }) => {
+
+    });
 
     return (
         <div>cell-list</div>
