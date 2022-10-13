0083653d818293cdf7ee5a8d99ee24c90ebf5f88
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-13 08:29:48 +0300

    Removed the import for the code cell component

--- browser-transpiler/src/App.tsx
+++ browser-transpiler/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import CodeCell from './components/code-cell';
 import { Provider } from 'react-redux';
 import { store } from './state';
 import CellList from './components/cell-list';
