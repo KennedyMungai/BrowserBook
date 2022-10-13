f150260772f48362caf2be17519129288b1aa02b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-13 08:43:59 +0300

    Received the useTypedSelector as a variable called cells

--- browser-transpiler/src/components/cell-list.tsx
+++ browser-transpiler/src/components/cell-list.tsx
@@ -3,7 +3,7 @@ import { useTypedSelector } from '../hooks/use-typed-selector';
 
 
 const CellList: React.FC = () => {
-    useTypedSelector(({ cells: { order, data } }) => {
+    const cells = useTypedSelector(({ cells: { order, data } }) => {
         return order.map((id) => {
             return data[id];
         });
