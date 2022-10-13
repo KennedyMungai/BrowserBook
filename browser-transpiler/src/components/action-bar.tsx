a1d3b97b6ebc096fa39d376ab8af3cb5059c4116
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-13 11:07:09 +0300

    Provided logic to the onClick event handlers

--- browser-transpiler/src/components/action-bar.tsx
+++ browser-transpiler/src/components/action-bar.tsx
@@ -12,9 +12,9 @@ const ActionBar: React.FC<ActionBarProps> = ({ id }) => {
 
     return (
         <div>
-            <button onClick={ }>Up</button>
-            <button onClick={ }>Down</button>
-            <button onClick={ }>Delete</button>
+            <button onClick={() => moveCell(id, 'up')}>Up</button>
+            <button onClick={() => moveCell(id, 'down')}>Down</button>
+            <button onClick={() => deleteCell(id)}>Delete</button>
         </div>
     )
 }
