2146a076406b578ea59ed787252ec262be305b5d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-14 08:36:44 +0300

    Added an import for fragment from react

--- browser-transpiler/src/components/cell-list.tsx
+++ browser-transpiler/src/components/cell-list.tsx
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react';
-import React from 'react';
+import React, { Fragment } from 'react';
 import { useTypedSelector } from '../hooks/use-typed-selector';
 import AddCell from './add-cell';
 import CellListItem from './cell-list-item';
