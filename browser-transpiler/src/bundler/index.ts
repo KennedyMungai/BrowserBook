ba4c6fde37159475ae17b508fdae1484c63e11fe
KennedyMungai <kennedymungaifmab@gmail.com>
2022-10-26 12:39:30 +0300

    Added the jsxFactory and the jsxFragment options

--- browser-transpiler/src/bundler/index.ts
+++ browser-transpiler/src/bundler/index.ts
@@ -6,15 +6,18 @@ import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 let service: esbuild.Service;
 
 
-const bundle = async (rawCode: string) => {
-    if (!service) {
+const bundle = async (rawCode: string) =>
+{
+    if (!service)
+    {
         service = await esbuild.startService({
             worker: true,
             wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
         });
     }
 
-    try {
+    try
+    {
         const result = await service.build({
             entryPoints: ['index.js'],
             bundle: true,
@@ -26,14 +29,17 @@ const bundle = async (rawCode: string) => {
             define: {
                 'process.env.NODE_ENV': '"production"',
                 global: 'window'
-            }
+            },
+            jsxFactory: '_React.createElement',
+            jsxFragment: '_React.Fragment'
         });
 
         return {
             code: result.outputFiles[0].text,
             err: ''
         };
-    } catch (err: any) {
+    } catch (err: any)
+    {
         return {
             code: '',
             err: err.message
