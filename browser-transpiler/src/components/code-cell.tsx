9abf6a99dee06bee19ce51f1b8af798dfd7e442a
KennedyMungai <kennedymungaifmab@gmail.com>
2022-10-26 12:51:04 +0300

    Cleared the cumulative loop array

--- browser-transpiler/src/components/code-cell.tsx
+++ browser-transpiler/src/components/code-cell.tsx
@@ -52,33 +52,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) =>
 
         const showFuncNoOp = 'var show = () => {}';
 
-        const cumulativeCode = [
-            `
-                import _React from 'react';
-                import _ReactDOM from 'react-dom';
-
-
-                const show = (value) => {
-                    const root = document.querySelector('#root');
-
-                    if(typeof value === 'object')
-                    {
-                        if(value.$$typeof && value.props)
-                        {
-                            _ReactDOM.render(value, root);
-                        }
-                        else
-                        {
-                            root.innerHTML = JSON.stringify(value);
-                        }
-                    }
-                    else
-                    {
-                        root.innerHTML = value;
-                    }
-                }
-            `
-        ];
+        const cumulativeCode = [];
 
         for (let c of orderedCells)
         {
