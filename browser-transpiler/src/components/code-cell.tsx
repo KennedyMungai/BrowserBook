6e79ee323c36274d69b9d1e6f2f8e7172bb6e859
KennedyMungai <kennedymungaifmab@gmail.com>
2022-10-26 10:36:27 +0300

    Changed the mark up for the react component

--- browser-transpiler/src/components/code-cell.tsx
+++ browser-transpiler/src/components/code-cell.tsx
@@ -49,19 +49,19 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) =>
                         onChange={(value) => updateCell(cell.id, value)}
                     />
                 </Resizable>
-
-                {
-                    !bundle || bundle.loading
-                        ? <div className="progress-wrapper">
+                <div className="progress-wrapper">
+                    {
+                        !bundle || bundle.loading
+                            ?
                             <div className="progress-cover">
                                 <progress className='progress is-small is-primary' max="100">
                                     Loading
                                 </progress>
                             </div>
-                        </div>
 
-                        : <Preview code={bundle.code} err={bundle.err} />
-                }
+                            : <Preview code={bundle.code} err={bundle.err} />
+                    }
+                </div>
             </div>
         </Resizable >
     );
