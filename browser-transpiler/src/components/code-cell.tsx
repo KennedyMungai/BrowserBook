87a8f20d25abff232b59ae9dafc50c68b9c836b0
KennedyMungai <kennedymungaifmab@gmail.com>
2022-10-26 10:31:04 +0300

    Wrapped the elementsdinside the progress-cover class with a div with the css class of progress-wrapper

--- browser-transpiler/src/components/code-cell.tsx
+++ browser-transpiler/src/components/code-cell.tsx
@@ -52,11 +52,14 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) =>
 
                 {
                     !bundle || bundle.loading
-                        ? <div className="progress-cover">
-                            <progress className='progress is-small is-primary' max="100">
-                                Loading
-                            </progress>
+                        ? <div className="progress-wrapper">
+                            <div className="progress-cover">
+                                <progress className='progress is-small is-primary' max="100">
+                                    Loading
+                                </progress>
+                            </div>
                         </div>
+
                         : <Preview code={bundle.code} err={bundle.err} />
                 }
             </div>
