41e53b0199a9c5b7a709b84bcc203dab74aca845
KennedyMungai <kennedymungaifmab@gmail.com>
2022-10-26 08:50:14 +0300

    The statement returns immediately afterwards

--- browser-transpiler/src/components/code-cell.tsx
+++ browser-transpiler/src/components/code-cell.tsx
@@ -24,6 +24,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) =>
         if (!bundle)
         {
             createBundle(cell.id, cell.content);
+            return;
         }
 
         const timer = setTimeout(async () =>
